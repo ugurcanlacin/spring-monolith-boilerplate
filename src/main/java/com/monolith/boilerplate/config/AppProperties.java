9a35d6d0b94a9e474c476a3aacfe9e4e6799d487
Ugurcan Lacin <ugurcanlacin@gmail.com>
2020-02-08 14:23:40 +0100

    Refactored app properties object and added tests

--- src/main/java/com/monolith/boilerplate/config/AppProperties.java
+++ src/main/java/com/monolith/boilerplate/config/AppProperties.java
@@ -1,54 +1,28 @@
 package com.monolith.boilerplate.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
 public class AppProperties {
     private final Auth auth = new Auth();
     private final OAuth2 oauth2 = new OAuth2();
 
+    @Getter
+    @Setter
     public static class Auth {
         private String tokenSecret;
         private long tokenExpirationMsec;
-
-        public String getTokenSecret() {
-            return tokenSecret;
-        }
-
-        public void setTokenSecret(String tokenSecret) {
-            this.tokenSecret = tokenSecret;
-        }
-
-        public long getTokenExpirationMsec() {
-            return tokenExpirationMsec;
-        }
-
-        public void setTokenExpirationMsec(long tokenExpirationMsec) {
-            this.tokenExpirationMsec = tokenExpirationMsec;
-        }
     }
 
+    @Getter
+    @Setter
     public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = new ArrayList<>();
-
-        public List<String> getAuthorizedRedirectUris() {
-            return authorizedRedirectUris;
-        }
-
-        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
-            this.authorizedRedirectUris = authorizedRedirectUris;
-            return this;
-        }
-    }
-
-    public Auth getAuth() {
-        return auth;
-    }
-
-    public OAuth2 getOauth2() {
-        return oauth2;
+        private List<String> authorizedRedirectUris;
     }
 }
--- src/test/java/com/monolith/boilerplate/BoilerplateApplicationTests.java
+++ src/test/java/com/monolith/boilerplate/BoilerplateApplicationTests.java
@@ -1,13 +1,29 @@
 package com.monolith.boilerplate;
 
+import com.monolith.boilerplate.config.AppProperties;
+import lombok.AllArgsConstructor;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = BoilerplateApplication.class)
 class BoilerplateApplicationTests {
+
+	@Autowired
+	private AppProperties appProperties;
 
 	@Test
 	void contextLoads() {
+		Assert.assertNotNull(appProperties.getAuth());
+		Assert.assertNotNull(appProperties.getAuth().getTokenSecret());
+		Assert.assertNotNull(appProperties.getAuth().getTokenExpirationMsec());
+		Assert.assertNotNull(appProperties.getOauth2());
+		Assert.assertNotNull(appProperties.getOauth2().getAuthorizedRedirectUris());
+		Assert.assertNotEquals(0, appProperties.getOauth2().getAuthorizedRedirectUris().size());
 	}
 
 }
