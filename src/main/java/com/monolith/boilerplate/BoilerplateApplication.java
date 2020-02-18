da00e5011b4ec6175eae47ced26f96d02438efca
Ugurcan Lacin <ugurcan.lacin@mirado.com>
2020-02-18 09:30:14 +0100

    Enabled JPA Auditing

--- src/main/java/com/monolith/boilerplate/BoilerplateApplication.java
+++ src/main/java/com/monolith/boilerplate/BoilerplateApplication.java
@@ -4,9 +4,11 @@ import com.monolith.boilerplate.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+@EnableJpaAuditing
 public class BoilerplateApplication {
 
 	public static void main(String[] args) {
