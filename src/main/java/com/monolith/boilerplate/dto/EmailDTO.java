113e3a67b1e19fa823640c64be0e6bc9872f1b94
ugurcanlacin <ugurcanlacin@gmail.com>
2020-03-02 00:10:03 +0100

    Integrated ActiveMQ JMS for email sending

---
+++ src/main/java/com/monolith/boilerplate/dto/EmailDTO.java
@@ -0,0 +1,18 @@
+package com.monolith.boilerplate.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDTO {
+    private String to;
+    private String subject;
+    private String text;
+}
--- src/main/java/com/monolith/boilerplate/service/EmailService.java
+++ src/main/java/com/monolith/boilerplate/service/EmailService.java
@@ -1,6 +1,9 @@
 package com.monolith.boilerplate.service;
 
+import com.monolith.boilerplate.dto.EmailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,12 +13,23 @@ public class EmailService {
     @Autowired
     public JavaMailSender emailSender;
 
-    public void sendSimpleMessage(
-            String to, String subject, String text) {
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    private void sendEmail(EmailDTO email) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
+        message.setTo(email.getTo());
+        message.setSubject(email.getSubject());
+        message.setText(email.getText());
         emailSender.send(message);
+    }
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(EmailDTO email) {
+        sendEmail(email);
+    }
+
+    public void sendEmailToQueue(EmailDTO email){
+        jmsTemplate.convertAndSend("mailbox", email);
     }
 }
