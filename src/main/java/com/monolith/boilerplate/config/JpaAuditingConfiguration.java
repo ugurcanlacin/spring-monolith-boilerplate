1937cfcf90654ad8fd048a8b22461d5798df9e3a
ugurcanlacin <ugurcanlacin@gmail.com>
2020-04-13 17:50:05 +0200

    Refactoring JpaAuditing

--- src/main/java/com/monolith/boilerplate/config/JpaAuditingConfiguration.java
+++ src/main/java/com/monolith/boilerplate/config/JpaAuditingConfiguration.java
@@ -4,19 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfiguration {
-
     @Bean
     public AuditorAware<String> auditorProvider() {
-        /*
-          if you are using spring security, you can get the currently logged username with following code segment.
-          SecurityContextHolder.getContext().getAuthentication().getName()
-         */
-        return () -> Optional.ofNullable("ugurcan");
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
     }
 }
